79b92b6f9e26a4987b98a9d39a000799556aadd3
Soma Sekhar <ssnani192@gmail.com>
2026-06-25 01:11:48 +0530

    Refactor search test comments for clarity

--- week1/Algorithms_Data Structures/E-commerce Platform Search Function/Main.java
+++ week1/Algorithms_Data Structures/E-commerce Platform Search Function/Main.java
@@ -11,14 +11,14 @@ public class Main {
             new Product(105, "Watch", "Accessories")
         };
 
-        // Linear Search Test
+        // Linear Search
         Product foundLinear = Search.linearSearch(products, "Phone");
         System.out.println("Linear Search Result: " + (foundLinear != null ? foundLinear : "Not Found"));
 
         // Sort for Binary Search
         Arrays.sort(products, Comparator.comparing(p -> p.productName.toLowerCase()));
 
-        // Binary Search Test
+        // Binary Search 
         Product foundBinary = Search.binarySearch(products, "Phone");
         System.out.println("Binary Search Result: " + (foundBinary != null ? foundBinary : "Not Found"));
     }
